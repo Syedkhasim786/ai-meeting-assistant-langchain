8b9b76d2cf8e293df2b5ddc58a039073b58021b4
Syedkhasim786 <syedkhasim2001@gmail.com>
2026-04-06 14:01:19 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,10 +1,9 @@
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI
 
 chain = None
 
@@ -14,7 +13,7 @@ def create_chain(text):
     splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=50)
     docs = splitter.create_documents([text])
 
-    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     vectorstore = FAISS.from_documents(docs, embeddings)
     retriever = vectorstore.as_retriever()
 
