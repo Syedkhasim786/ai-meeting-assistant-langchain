7326c526314a8954157b96842325251730590079
Syedkhasim786 <syedkhasim2001@gmail.com>
2026-04-06 12:14:04 +0530

    Create rag.py

---
+++ rag.py
@@ -0,0 +1,26 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
+
+qa_chain = None
+
+def create_chain(text):
+    global qa_chain
+
+    splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+    docs = splitter.create_documents([text])
+
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectorstore = FAISS.from_documents(docs, embeddings)
+
+    llm = ChatOpenAI(model="gpt-4o-mini")
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=vectorstore.as_retriever()
+    )
+
+def ask_question(query):
+    return qa_chain.run(query)
